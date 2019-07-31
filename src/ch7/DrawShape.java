bdbbff439d61fd788eb142ac31a1e618c3c32545
anjim <anjim@DESKTOP-HBH5TST.T-mobile.com>
2019-07-31 16:55:42 +0900

    backup

--- src/ch7/DrawShape.java
+++ src/ch7/DrawShape.java
@@ -1,11 +1,18 @@
-package ch7;
-
 public class DrawShape {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		Point[] p = {	new Point(100, 100),
+						new Point(140, 50),
+						new Point(200, 100)
+					};
+		
+		Triangle t = new Triangle(p);
+		Circle c = new Circle(new Point(150, 150), 50);
+		
+		t.draw();
+		c.draw();
+		}
+	
 
 }
 
